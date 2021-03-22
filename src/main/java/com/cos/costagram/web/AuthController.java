21bebe637e827d0e836d531732be156cbebcf813
kimhaengun <https://github.com/kimhaengun>
2021-03-22 11:12:04 +0900

    1.인스타그램 화면 구성

---
+++ src/main/java/com/cos/costagram/web/AuthController.java
@@ -0,0 +1,19 @@
+package com.cos.costagram.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// 시작 주소 : /auth
+@Controller
+public class AuthController {
+	
+	@GetMapping("/auth/loginForm")
+	public String loginForm() {
+		return "auth/loginForm";
+	}
+	
+	@GetMapping("/auth/joinForm")
+	public String joinForm() {
+		return "auth/joinForm";
+	}
+}
---
+++ src/main/java/com/cos/costagram/web/UserController.java
@@ -0,0 +1,15 @@
+package com.cos.costagram.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/test")
+	public String test() {
+		return "index";
+	}
+	
+}
