857c2f6c7432cb68f440ab2d68f1d3d752b9b923
kimhaengun <https://github.com/kimhaengun>
2021-03-22 12:38:57 +0900

    2.인스타그램 클론코딩 - 시큐리티 설정 완료(Oauth2.0, CORS설정은 추후 진행)

---
+++ src/main/java/com/cos/costagram/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.cos.costagram.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean
+	public BCryptPasswordEncoder encode() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	//모델 : Image, User, Likes, Follow, Tag : 인증 필요
+	//auth 주소 : 인증 필요없음
+	//static 폴더 (이미지)
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable();
+//		http.cors().disable();//컨트롤러에 CrossOrign을 붙여도, 시큐리티에서 막힘./
+		http.authorizeRequests()
+		.antMatchers("/", "/user/**", "/image/**", "/follow/**").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+		.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+		.anyRequest()
+		.permitAll()
+		.and()
+		.formLogin()
+		.loginPage("/auth/loginForm") //로그인 페이지로 보내고
+		.loginProcessingUrl("/"); //로그인 완료되면 feed페이지로 보냄.
+		//Oauth2.0과 CORS는 나중에 
+	}
+}
