43a35599b457dcc448e3e372a3aa701148490e01
kimhaengun <https://github.com/kimhaengun>
2021-03-23 12:32:23 +0900

    7.인스타그램 클론코딩-Tag,Likes,Image 모델 생성완료

--- src/main/java/com/cos/costagram/domain/user/User.java
+++ src/main/java/com/cos/costagram/domain/user/User.java
@@ -39,7 +39,8 @@ public class User {
 	private String email;
 	private String phone;
 	private String gender;
-	private String profileImage;
+	
+	private String profileImageUrl;
 	private String provider; // 제공자 Google, Facebook, Naver
 	
 	private String role; //USER,ADMIN
